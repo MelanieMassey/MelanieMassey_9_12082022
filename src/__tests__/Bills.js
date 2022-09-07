270079a1cb4ba39bb0181c478ee4482cee22db7c
Melanie Massey <nanie_d_enfer@hotmail.com>
2022-09-07 20:26:28 +0200

    Finalisation test d'intégration GET pour Bills.js

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -4,7 +4,7 @@
 
 // import {screen, waitFor} from "@testing-library/dom"
 import "@testing-library/jest-dom"
-import { screen, getByTestId, getAllByTestId, getByText, waitFor } from "@testing-library/dom"
+import { fireEvent, screen, getByTestId, getAllByTestId, getByText, waitFor } from "@testing-library/dom"
 import userEvent from "@testing-library/user-event"
 
 import BillsUI from "../views/BillsUI.js"
@@ -15,6 +15,9 @@ import { ROUTES_PATH} from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store"
 import router from "../app/Router.js";
+
+// INFO -- jest.mock() = Permet de tester sans toucher à l'API (et donc créer des tests lents et fragiles)
+jest.mock("../app/store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -135,22 +138,29 @@ describe("Given I am connected as an employee", () => {
 describe("Given I am a user connected as Employee", () => {
   describe("When I navigate to Bills page", () => {
     test("fetches bills from mock API GET", async () => {
-      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
+      
+
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
       router()
       window.onNavigate(ROUTES_PATH.Bills)
       // 
-      await waitFor(() => screen.getByText("Mes notes de frais"))
+      const pageTitle = await waitFor(() => screen.getByText("Mes notes de frais"))
+      expect(pageTitle).toBeTruthy()
       // const contentPending  = await screen.getByText("En attente (1)")
       // expect(contentPending).toBeTruthy()
       // const contentRefused  = await screen.getByText("Refusé (2)")
       // expect(contentRefused).toBeTruthy()
       // expect(screen.getByTestId("big-billed-icon")).toBeTruthy()
     })
+  })
   describe("When an error occurs on API", () => {
+    // INFO -- beforeEach() est exécuté avant chaque test
     beforeEach(() => {
+      // INFO -- jest.spyOn(object, methodName) = fonction simulée. Crée une fonction simulée similaire à jest.fn mais qui surveille également les appels à objet[methodName]. Retourne une fonction simulée de Jest.
       jest.spyOn(mockStore, "bills")
       Object.defineProperty(
           window,
@@ -161,22 +171,24 @@ describe("Given I am a user connected as Employee", () => {
         type: 'Employee',
         email: "a@a"
       }))
+
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.appendChild(root)
       router()
     })
-    test("fetches bills from an API and fails with 404 message error", async () => {
 
+    test("fetches bills from an API and fails with 404 message error", async () => {
+      // INFO - mockImplementationOnce() = Permete de recréer un comportement complexe d'une fonction simulée, de sorte que plusieurs appels de fonction produisent des résultats différents
       mockStore.bills.mockImplementationOnce(() => {
         return {
           list : () =>  {
             return Promise.reject(new Error("Erreur 404"))
           }
         }})
-      window.onNavigate(ROUTES_PATH.Dashboard)
+      window.onNavigate(ROUTES_PATH.Bills)
       await new Promise(process.nextTick);
-      const message = await screen.getByText(/Erreur 404/)
+      const message = await waitFor(() => screen.getByText(/Erreur 404/))
       expect(message).toBeTruthy()
     })
 
@@ -187,7 +199,8 @@ describe("Given I am a user connected as Employee", () => {
           list : () =>  {
             return Promise.reject(new Error("Erreur 500"))
           }
-        }})
+        }
+      })
 
       window.onNavigate(ROUTES_PATH.Dashboard)
       await new Promise(process.nextTick);
@@ -196,5 +209,5 @@ describe("Given I am a user connected as Employee", () => {
     })
   })
 
-  })
+  
 })
